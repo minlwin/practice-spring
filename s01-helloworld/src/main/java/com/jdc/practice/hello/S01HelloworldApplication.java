ecbbe74f01aa138a9fab221fb075096aeda84891
Min Lwin <lwin.zawmin@gmail.com>
2024-07-22 02:14:17 +0630

    Share Initial Projects

---
+++ s01-helloworld/src/main/java/com/jdc/practice/hello/S01HelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.practice.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S01HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S01HelloworldApplication.class, args);
+	}
+
+}
---
+++ s01-helloworld/src/test/java/com/jdc/practice/hello/S01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.practice.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S01HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
