646e6665412a7ebbbd6b0468102e09a63d4c241c
Min Lwin <lwin.zawmin@gmail.com>
2024-10-18 23:08:56 +0630

    Create Earthdistance

---
+++ s03-earthdistance/src/main/java/com/jdc/earthdistance/S03EarthdistanceApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.earthdistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S03EarthdistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S03EarthdistanceApplication.class, args);
+	}
+
+}
---
+++ s03-earthdistance/src/test/java/com/jdc/earthdistance/S03EarthdistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.earthdistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S03EarthdistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
