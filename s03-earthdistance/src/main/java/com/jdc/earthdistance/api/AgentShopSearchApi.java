31c825e7143a98c67dac670b288b124117ca3fd3
Min Lwin <lwin.zawmin@gmail.com>
2024-12-17 00:52:41 +0630

    Update Docker Compose File

--- s03-earthdistance/src/main/java/com/jdc/earthdistance/api/AgentShopSearchApi.java
+++ s03-earthdistance/src/main/java/com/jdc/earthdistance/api/AgentShopSearchApi.java
@@ -1,10 +1,24 @@
 package com.jdc.earthdistance.api;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jdc.earthdistance.api.input.AgentShopSearch;
+import com.jdc.earthdistance.api.output.AgentShopInfo;
+import com.jdc.earthdistance.model.service.AgentShopService;
+
 @RestController
-@RequestMapping("shops/manage")
+@RequestMapping("shops/nearby")
 public class AgentShopSearchApi {
+	
+	private AgentShopService service;
+
+	@GetMapping
+	List<AgentShopInfo> search(AgentShopSearch search) {
+		return service.search(search);
+	}
 
 }
--- s03-earthdistance/src/main/java/com/jdc/earthdistance/model/service/AgentShopService.java
+++ s03-earthdistance/src/main/java/com/jdc/earthdistance/model/service/AgentShopService.java
@@ -1,8 +1,18 @@
 package com.jdc.earthdistance.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.jdc.earthdistance.api.input.AgentShopSearch;
+import com.jdc.earthdistance.api.output.AgentShopInfo;
 
 @Service
 public class AgentShopService {
+
+	public List<AgentShopInfo> search(AgentShopSearch search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
